5e5796918b99371f8e0b4ceab954320c52695c67
mamedun <rikistudio90@gmail.com>
2024-05-31 15:33:03 +0700

    first commit

---
+++ js/produk.js
@@ -0,0 +1,59 @@
+var data_mentah = {
+    "Kopi": [
+        {
+            "foto": "images/produk/product_capuchino.jpg",
+            "nama": "Cappuccino",
+            "size": "200Ml",
+            "harga": "15.000",
+            "link": "#"
+        },
+        {
+            "foto": "images/produk/product_espresso.jpg",
+            "nama": "Espresso",
+            "size": "100 Ml",
+            "harga": "17.000",
+            "link": "#"
+        },
+        {
+            "foto": "images/produk/product_latte.jpg",
+            "nama": "Latte",
+            "size": "200 Ml",
+            "harga": "19.000",
+            "link": "#"
+        },
+        {
+            "foto": "images/produk/product_kopi_susu.jpg",
+            "nama": "Kopi Susu",
+            "size": "200 Ml",
+            "harga": "20.000",
+            "link": "#"
+        }
+    ],
+    "Gorengan": [
+        {
+            "foto": "https://www.blibli.com/friends-backend/wp-content/uploads/2023/03/B300012-Cover-resep-pisang-goreng-1024x683.jpg",
+            "nama": "Pisang Goreng",
+            "size": "1 Pcs",
+            "harga": "1.000",
+            "link": "#"
+        },
+        {
+
+            "foto": "https://asset.kompas.com/crops/7s1zIClbCexXibeqFVyVVgoWZGw=/4x2:972x648/750x500/data/photo/2022/09/21/632aa5001639f.jpg",
+
+            "nama": "Tempe Mendoan",
+            "size": "1 Pcs",
+            "harga": "1.000",
+            "link": "#"
+        },
+        {
+            "foto": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMIQRutnzAMLLVvg_SaRy0BTBAmiOfbo_1zFGuor8M_LKWy9Kn0xeCjCvD&s=10",
+            "nama": "Kentang Goreng",
+            "size": "1 porsi",
+            "harga": "10.000",
+            "link": "#"
+        }
+    ]
+}
+var data_yang_sudah_difilter = []
+console.log("Data Mentah:", data_mentah)
